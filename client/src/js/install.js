93b263ef23bc3a42186a3abb89cc6dee8b525e70
JoshuaToback <joshuatoback@gmail.com>
2023-02-06 15:09:28 -0500

    Added database and install, still configuring webpack

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,38 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit');
+    console.log("event", event);
+    event.preventDefault();
+
+    // Stores Triggered Events
+    window.deferredPrompt = event;
+
+    // Removes Hidden Class from Button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // Shows prompt
+    promptEvent.prompt();
+
+    // Resets the Deffered prompt variable, as it can only be used once. 
+    window.deferredPrompt = null;
+
+    butInstall.classlist.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+    console.log('install hit')
+    window.deferredPrompt = null;
+});
