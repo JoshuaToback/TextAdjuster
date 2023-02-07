399dddcc8c8763ac963ef1e0bd7f7d4bb7e34c32
JoshuaToback <joshuatoback@gmail.com>
2023-02-07 16:18:40 -0500

    Fixed connection issue and ports

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,15 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,19 +21,19 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // Set up asset cache
 registerRoute(
-  // Ddefine the callback function that filters the requests we want to cache (in this case, JS and CSS files)
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
     // Name of the cache storage.
-    cacheName: 'asset-cache',
+    cacheName: "asset-cache",
     plugins: [
       // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
@@ -41,7 +42,7 @@ registerRoute(
       new ExpirationPlugin({
         maxEntries: 60,
         maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
-      })
+      }),
     ],
   })
 );
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,39 +1,34 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('hit');
-    console.log("event", event);
+    console.log('hit')
+    console.log("event" + event)
     event.preventDefault();
-
-    // Stores Triggered Events
+    // Store the triggered events
     window.deferredPrompt = event;
 
-    // Removes Hidden Class from Button
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-
+    // console.log(promptEvent)
     if (!promptEvent) {
         return;
     }
 
-    // Shows prompt
+    // Show prompt
     promptEvent.prompt();
 
-    // Resets the Deffered prompt variable, as it can only be used once. 
+    // Reset the deferred prompt variable, it can only be used once.
     window.deferredPrompt = null;
 
-    butInstall.classlist.toggle('hidden', true);
+    butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
     console.log('install hit')
     window.deferredPrompt = null;
-});
+}); 
